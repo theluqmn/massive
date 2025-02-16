5a2184f803eda25d1e112a460439929379fd1368
theluqmn <luqman.yazrie@outlook.com>
2025-02-16 08:42:20 +0800

    basic COBOL done

---
+++ main.cbl
@@ -0,0 +1,18 @@
+      *This is a body mass index (BMI) calculator program. 
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "BMICALCULATOR".
+       AUTHOR. "Luqman".
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WEIGHT_KG PIC 999.
+       01 HEIGHT_CM PIC 999.
+       01 BMI PIC 999V99.
+
+       PROCEDURE DIVISION.
+
+
+       
+       STOP RUN.
+       END PROGRAM BMICALCULATOR.
